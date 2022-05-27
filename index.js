e1c10fca9f4c5fd1db32c0d7966c952363594462
Jeihan Praja Yudanto <jeihan.yudanto@gmail.com>
2022-05-27 17:16:20 +0700

    latihan object

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const sheet = [];
+const kucing = {
+    nama: "Whiskas",
+    warna: "Hitam",
+    jumlahKaki: 4
+};
 
-//deklarasi baris pada array
-sheet[0] = [];
-sheet[1] = [];
-sheet[2] = [];
+const kucing2 = new Object();
 
-//deklarasi kolom pada array
-sheet[0][0] = 100; sheet[0][1] = 200; sheet[0][2] = 300;
-sheet[1][0] = 400; sheet[1][1] = 500; sheet[1][2] = 600;
-sheet[2][0] = 700; sheet[2][1] = 800; sheet[2][2] = 900;
+kucing2.nama = "Me-O";
+kucing2.warna = "Belang";
+kucing2.jumlahKaki = 4;
 
-console.log(sheet);
+console.log(kucing);
+console.log(kucing2);
